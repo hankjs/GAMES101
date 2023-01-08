dce894d4b9ebde62967f4fcbca5216d9704cc81d
hank <hank-c@qq.com>
2023-01-08 22:39:44 +0800

    feat: add eigen3

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+workspace "GAMES101"
+	architecture "x64"
+	startproject "Main"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = ""
+
+IncludeDir = {}
+
+project "Eigen"
+	location "Eigen"
+
+project "*"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetname ("Main")
+	targetdir ("%{wks.location}/%{prj.name}/bin/" .. outputdir)
+	objdir    ("%{wks.location}/%{prj.name}/bin-int/" .. outputdir)
+
+	files {
+		"%{wks.location}/%{prj.name}/src/**.h",
+		"%{wks.location}/%{prj.name}/src/**.cpp",
+	}
+
+	defines {
+		"_WIN32"
+	}
+
+	includedirs {
+		"%{wks.location}/%{prj.name}/src",
+		"%{wks.location}/vendor"
+	}
+
+	links { 
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+
+		
